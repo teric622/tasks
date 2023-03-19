2e2d2b7a47ca3d9b2f178f665daf3e370f5da661
teric622 <tlasecaeric@gmail.com>
2023-03-19 16:33:20 -0400

    changetype and reveal answer working

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,41 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
+/**
+   provides a way to change the type of a quiz question by pressing a button. 
+ * Recall from the Objects task that Quiz Questions can come in either Multiple Choice or Short Answer.
+    You will need a single state to handle whether the type is multiple_choice_question or short_answer_question.
+    The type of the state should be QuestionType, not string.
+    There should be a button labelled Change Type 
+    that changes the state from one type to the other.
+
+    When the type is multiple_choice_question, the text Multiple Choice should be visible.
+    When the type is short_answer_question, the text Short Answer should be visible.
+    The initial type must be short_answer_question.
+ *  
+
+    BASICALLY: make button that when clicked changes question type and displays whatever type 
+ */
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "multiple_choice_question"
+    );
+
+
+    const changeQuestion = (): void => {
+        console.log("before");
+        console.log(questionType);
+        setQuestionType(
+            questionType === "short_answer_question" ? "multiple_choice_question": "short_answer_question"
+        );
+        console.log("after");
+        console.log(questionType);
+    };
+    return (<div>
+        {/* <div>{questionType}</div>; */}
+        <Button id="Change Type" onClick={changeQuestion}>Change Type</Button>;
+        <div>{questionType == "short_answer_question" ? "Multiple Choice":"Short Answer"}</div>;
+    </div>);
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -1,6 +1,57 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+/**
+ * The StartAttempt component simulates yet 
+ * another part of the quiz application, 
+ * this time providing a “Start” and “Stop” button for the quiz.
+ *  Since the quiz should have a limited number of attempts, 
+ * there is also a “Mulligan” button to give more attempts.
+
+You will need two pieces of state: the number of attempts and whether the quiz is in progress.
+The initial number of attempts is 4
+The quiz is initially NOT in progress
+There is a button labelled Start Quiz that puts the Quiz in progress and decreases the number of attempts by one.
+There is a button labelled Stop Quiz that stops the Quiz from being in progress.
+There is a button labelled Mulligan that increase the attempts by one.
+When the quiz is in progress, the Start Quiz and Mulligan buttons are disabled.
+When the quiz is not in progress, the Stop Quiz button is disabled.
+When the attempts are zero, the Start Quiz button is disabled.
+ * <Button onClick={()=>setDriving(true)} disabled={driving}>Drive</Button>
+ */
+
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState(4);
+    const [progress, setProgress] = useState(false);
+
+    const increment = (): void => {
+        setAttempts(attempts + 1);
+    };
+    const decrement = (): void => {
+        console.log(attempts - 1);
+        setAttempts(attempts - 1);
+        setProgress(true);
+    };
+
+    return (
+        <div>
+            <div>{attempts}</div>
+            <Button id="Start Quiz" onClick={decrement} disabled={progress}>
+                Start Quiz
+            </Button>
+            ;
+            <Button
+                id="Stop Quiz"
+                onClick={() => setProgress(false)}
+                disabled={!progress}
+            >
+                Stop Quiz
+            </Button>
+            ;
+            <Button id="Mulligan" onClick={increment} disabled={progress}>
+                Mulligan
+            </Button>
+            ;
+        </div>
+    );
 }
