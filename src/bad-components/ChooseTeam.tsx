bffb354739741ec4b2f7b807c7e2ff5c30fb45f3
teric622 <tlasecaeric@gmail.com>
2023-03-24 11:54:36 -0400

    updated task9

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -35,7 +35,7 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const [allOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
     function chooseMember(person: string) {
